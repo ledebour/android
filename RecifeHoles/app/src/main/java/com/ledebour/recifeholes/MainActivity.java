4959794cfb0c006761785482722b177e72b6ecf8
Luís Ledebour <luis.ledebour@gmail.com>
2016-08-05 00:31:01 -0300

    Kick-off

---
+++ RecifeHoles/app/src/main/java/com/ledebour/recifeholes/MainActivity.java
@@ -0,0 +1,178 @@
+package com.ledebour.recifeholes;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener,SensorEventListener {
+
+    @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.fab) FloatingActionButton fab;
+    @BindView(R.id.drawer_layout) DrawerLayout drawer;
+    @BindView(R.id.nav_view) NavigationView navigationView;
+    @BindView(R.id.text_view_x) TextView text_view_x;
+    @BindView(R.id.text_view_y) TextView text_view_y;
+    @BindView(R.id.text_view_z) TextView text_view_z;
+    @BindView(R.id.text_view_detail) TextView text_view_detail;
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        setSupportActionBar(toolbar);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Float x = event.values[0];
+        Float y = event.values[1];
+        Float z = event.values[2];
+
+         /*
+        Os valores ocilam de -10 a 10.
+        Quanto maior o valor de X mais ele ta caindo para a esquerda - Positivo Esqueda
+        Quanto menor o valor de X mais ele ta caindo para a direita  - Negativo Direita
+        Se o valor de  X for 0 então o celular ta em pé - Nem Direita Nem Esquerda
+        Se o valor de Y for 0 então o cel ta "deitado"
+         Se o valor de Y for negativo então ta de cabeça pra baixo, então quanto menor y mais ele ta inclinando pra ir pra baixo
+        Se o valor de Z for 0 então o dispositivo esta reto na horizontal.
+        Quanto maioro o valor de Z Mais ele esta inclinado para frente
+        Quanto menor o valor de Z Mais ele esta inclinado para traz.
+        */
+        text_view_x.setText("Posição X: " + x.intValue() + " Float: " + x);
+        text_view_y.setText("Posição Y: " + y.intValue() + " Float: " + y);
+        text_view_z.setText("Posição Z: " + z.intValue() + " Float: " + z);
+
+        if(y < 0) { // O dispositivo esta de cabeça pra baixo
+            if(x > 0)
+                text_view_detail.setText("Virando para ESQUERDA ficando INVERTIDO");
+            if(x < 0)
+                text_view_detail.setText("Virando para DIREITA ficando INVERTIDO");
+        } else {
+            if(x > 0)
+                text_view_detail.setText("Virando para ESQUERDA ");
+            if(x < 0)
+                text_view_detail.setText("Virando para DIREITA ");
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+}
